b6537b191ef608a04bc674c19c09c0dfe122fa73
AliRanjbari <AliRanjbari86@gmail.com>
2023-02-26 15:26:00 +0330

    write entire functions and switch case.

--- src/main/java/edu/app/api/DB.java
+++ src/main/java/edu/app/api/DB.java
@@ -13,4 +13,45 @@ public class DB {
         this.commodities = new ArrayList<Commodity>();
         this.providers = new ArrayList<Provider>();
     }
+
+    void addUser(String jsonString) {
+        System.out.println("adding user");
+    }
+
+    void addProvider(String jsonString) {
+        System.out.println("adding provider");
+    }
+
+    void addCommodity(String jsonString) {
+        System.out.println("adding commodity");
+    }
+
+    String getCommodityList(String jsonString){
+        return "Commodity list";
+    }
+
+    void rateCommodity(String jsonString) {
+        System.out.println("rating commodity");
+    }
+
+    void addToBuyList(String jsonString) {
+        System.out.println("adding to buy list");
+    }
+
+    void removeFromBuyList(String jsonString) {
+        System.out.println("removing from buy list");
+    }
+
+    String getCommodityById(String jsonString) {
+        return "getting commodity by id [json format]";
+    }
+
+    String getCommoditiesByCategory(String jsonString) {
+        return "getting commodity by category [json format]";
+    }
+
+    String getBuyList(String jsonString) {
+        return "getting buy list [json format] ";
+    }
+
 }
--- src/main/java/edu/app/api/Handler.java
+++ src/main/java/edu/app/api/Handler.java
@@ -1,57 +1,72 @@
 package edu.app.api;
 
-
-import java.lang.reflect.Array;
-
 public class Handler {
 
-    DB dataBase;
+    DB dataBase = new DB();
     public void handleCommand(String input) {
         String[] arguments = input.split(" ");
+        if (arguments.length != 2){
+            System.err.println("Incorrect format have to be like [command] [json]");
+            return;
+        }
+
         String command = arguments[0];
+        String jsonString = arguments[1];
 
         switch (command) {
             case "addUser":
             {
+                dataBase.addUser(jsonString);
                 break;
             }
             case "addProvider":
             {
+                dataBase.addProvider(jsonString);
                 break;
             }
             case "addCommodity":
             {
+                dataBase.addCommodity(jsonString);
                 break;
             }
             case "getCommoditiesList":
             {
+                System.out.println(dataBase.getCommodityList(jsonString));
                 break;
             }
             case "rateCommodity":
             {
+                dataBase.rateCommodity(jsonString);
                 break;
             }
             case "addToBuyList":
             {
+                dataBase.addToBuyList(jsonString);
                 break;
             }
             case "removeFromBuyList":
             {
+                dataBase.removeFromBuyList(jsonString);
                 break;
             }
             case "getCommodityById":
             {
+                System.out.println(dataBase.getCommodityById(jsonString));
                 break;
             }
-            case "getCommodityByCategory":
+            case "getCommoditiesByCategory":
             {
+                System.out.println(dataBase.getCommoditiesByCategory(jsonString));
                 break;
             }
             case "getBuyList":
             {
+                System.out.println(dataBase.getBuyList(jsonString));
                 break;
             }
 
+            default:
+                System.err.println("Incorrect Command");
         }
 
     }
