f13b764afffe1b08ea13a67a51ee0f9221c65fe6
Amirreza Ghahremani <ghahremani.matinn@gmail.com>
2023-02-28 18:38:51 +0330

    test for duplicate rating by same person has added

--- src/test/java/RateTest.java
+++ src/test/java/RateTest.java
@@ -1,5 +1,6 @@
 import edu.app.api.Commodity;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 import java.util.ArrayList;
@@ -7,10 +8,32 @@ import java.util.Arrays;
 import java.util.List;
 
 public class RateTest {
+    private Commodity commodity;
+
+
+    @BeforeEach
+    public void initTest() {
+          commodity = new Commodity(10, "shoes", 5, 300,
+                                    new ArrayList<>(Arrays.asList("ALI", "MAMAD")), 0, 0);
+    }
+
     @Test
     public void testCommodityRate(){
-        Commodity commodity = new Commodity(10,"shoes",5,300,new ArrayList<>(Arrays.asList("ALI" , "MAMAD")) , 0 ,0 );
         commodity.rate("matin" , 5);
-        assertEquals(5,commodity.getRating());
+        commodity.rate("ali" , 7);
+        assertEquals(6,commodity.getRating());
     }
+
+    @Test
+    public void testDuplicateRate(){
+        commodity.rate("ali" , 4);
+        commodity.rate("majid" , 10);
+        commodity.rate("matin" , 10);
+        //till now the score is 8
+        commodity.rate("ali" , 1);
+        assertEquals(7,commodity.getRating());
+        //now the score should be 7 because ali's score has changed
+
+    }
+
 }
