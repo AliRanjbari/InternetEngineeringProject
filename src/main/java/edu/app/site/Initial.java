377257e2f304a4376e0e6d8beb2559bdeecbe6e4
AliRanjbari <AliRanjbari86@gmail.com>
2023-03-12 17:56:20 +0330

    base structure of Initial.

---
+++ src/main/java/edu/app/site/Initial.java
@@ -0,0 +1,31 @@
+package edu.app.site;
+
+import edu.app.api.DB;
+
+public class Initial {
+    String baseUrl = "http://5.253.25.110:5000/api";
+
+    public static void initDatabase(DB database) {
+
+    }
+
+    //Use json handler to fill database
+    private static void getUsers(DB database) {
+
+    }
+
+    private static void getCommodities(DB database) {
+
+    }
+
+    private static void getProviders(DB database) {
+
+    }
+
+    private static void getComments(DB database) {
+
+    }
+
+
+
+}
---
+++ src/main/java/edu/app/site/Site.java
@@ -0,0 +1,4 @@
+package edu.app.site;
+
+public class Site {
+}
