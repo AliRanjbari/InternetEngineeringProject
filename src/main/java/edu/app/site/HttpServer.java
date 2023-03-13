aa83745532d03c5abee6aefaa7a1d522a4c125f8
AliRanjbari <AliRanjbari86@gmail.com>
2023-03-13 15:10:47 +0330

    change in name and files.

---
+++ src/main/java/edu/app/site/HttpServer.java
@@ -0,0 +1,47 @@
+package edu.app.site;
+
+
+import edu.app.api.DB;
+import io.javalin.Javalin;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class HttpServer {
+    Javalin app;
+    DB database;
+
+    public HttpServer() {
+        this.database = new DB();
+        this.app  = Javalin.create(/*config*/)
+                .get("/", ctx -> ctx.result("Hello World"));
+
+        app.get("/index", ctx -> {
+           ctx.html(getFileContent("src/pages/index.html"));
+        });
+    }
+
+    private static String getFileContent(String fileName) {
+        try {
+            byte[] bytes = Files.readAllBytes(Paths.get(fileName));
+            return new String(bytes, StandardCharsets.UTF_8);
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "\"" + fileName + "\" file Not found!!!";
+        }
+    }
+
+
+    public void startServer(int port) {
+        this.app.start(port);
+    }
+
+    public static void main(String[] args) {
+        /*Javalin app  = Javalin.create(*//*config*//*)
+                .get("/", ctx -> ctx.result("Hello World"))
+                .start(7070);*/
+        //System.out.println(app.getClass().getName());
+    }
+}
---
+++ src/main/java/edu/app/site/PageBuilder.java
@@ -0,0 +1,19 @@
+package edu.app.site;
+
+import edu.app.api.User;
+import edu.app.api.Commodity;
+import org.jsoup.Jsoup;
+
+import java.util.List;
+
+public class PageBuilder {
+
+    static void createCommoditiesPage(List<Commodity> commodities) {
+
+    }
+
+    private static void createCommodityPage(Commodity commodity) {
+
+    }
+
+}
