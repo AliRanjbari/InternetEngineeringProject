35dfe6e9a34a58bc02ee3884b6e906826e98db9a
Amirreza Ghahremani <ghahremani.matinn@gmail.com>
2023-05-04 21:02:10 +0430

    no need imports delete

--- src/main/java/edu/app/Controller/ProviderController.java
+++ src/main/java/edu/app/Controller/ProviderController.java
@@ -1,22 +1,11 @@
 package edu.app.Controller;
 
-import edu.app.api.Commodity;
 import edu.app.api.Provider;
 import edu.app.service.BalootService;
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import static java.lang.Math.ceil;
 @RestController
 @RequestMapping("/provider")
 public class ProviderController extends HttpServlet{
