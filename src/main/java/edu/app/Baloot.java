6dccc0b11fd87ffa16945323d908cbad28026bac
Amirreza Ghahremani <ghahremani.matinn@gmail.com>
2023-06-02 23:04:27 +0330

    error for filling db

--- src/main/java/edu/app/Baloot.java
+++ src/main/java/edu/app/Baloot.java
@@ -1,10 +1,14 @@
 package edu.app;
 
+import edu.app.model.User;
 import edu.app.service.BalootService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.web.bind.annotation.RestController;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 
 @SpringBootApplication
@@ -14,6 +18,24 @@ public class Baloot {
 
         BalootService.getInstance();
         SpringApplication.run(Baloot.class, args);
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(User.class)
+                .buildSessionFactory();
+        Session session = factory.getCurrentSession();
+
+        try {
+            User user = BalootService.getInstance().getDatabase().getUsers().get(0);
+            session.beginTransaction();
+            session.save(user);
+            session.getTransaction().commit();
+            System.out.println("Data is Added");
+
+        } finally {
+            // TODO: handle finally clause
+            session.close();
+            factory.close();
+        }
     }
 
 }
